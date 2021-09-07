af83f2421c75c0e2f34ff44cfe2bb2fcddff7e21
Daniel <daniel.roa98@gmail.com>
2021-09-06 20:36:08 -0500

    Final version, hooks assignment
    This task was done cooperatively with Jorge Palacios

---
+++ entregable-2/src/App.js
@@ -0,0 +1,62 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
+import { Home,About, Dashboard } from "./components/NewInput";
+
+import HomeButton from "./components/HomeButton";
+
+export default function BasicExample() {
+
+  let dummyData = [
+    {
+      id: 1,
+      name: 'Daniel'
+    },
+    {
+      id: 2,
+      name: 'Jorge'
+    },
+    {
+      id: 3,
+      name: 'Antonio'
+    },
+    {
+      id: 4,
+      name: 'Sebas'
+    },
+  ]
+
+  return (
+
+    <Router>
+
+      <HomeButton />
+
+      <Link style={{ padding: "30px" }} to="/about">Hooks</Link>
+
+      <Link style={{ padding: "30px" }} to="/dashboard">User Params</Link>
+
+      <hr />
+
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/dashboard/:name">
+          <Dashboard data/>
+        </Route>
+      </Switch>
+
+    </Router>
+
+  );
+}
---
+++ entregable-2/src/components/NewInput.jsx
@@ -0,0 +1,109 @@
+import { React, useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+import InputButton from "./InputButton";
+
+function Home() {
+
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [hours, setHours] = useState(0);
+
+  useEffect(() => {
+    let interval = null;
+
+    interval = setInterval(() => {
+      if (seconds < 60) {
+        setSeconds(seconds => seconds + 1);
+      }
+      else {
+        setSeconds(0)
+        if (minutes < 60) {
+          setMinutes(minutes => minutes + 1);
+        }
+
+        else {
+          setSeconds(0);
+          setMinutes(0);
+          setHours(hours => hours + 1);
+        }
+      }
+    }, 1);
+
+    return () => clearInterval(interval);
+  });
+
+    return (
+      <div>
+        <h2>Timer</h2>
+        <h3>The current timer is: {hours}:{minutes}:{seconds}</h3>
+      </div>
+    );
+  }
+  
+  function About() {
+    const [users, setUsers] = useState([{ id: 0, name: "Equipo dinamita" }]);
+
+    const onClick = () => {
+      setUsers([...users, { id: users.length, name: "Entregable" }])
+    };
+
+    var dataComponent = {
+      type: 'button',
+      value: 'Update',
+      hidden: false
+    }
+
+    return (
+      <div>
+        <h2>About</h2>
+        <InputButton onClick={onClick} {...dataComponent} />
+        <div>
+          {users.map((e) => (
+            <div>
+              {e.id} - {e.name}
+            </div>
+          ))}
+        </div>
+      </div>
+  )
+  }
+
+  const Dashboard = ({data}) => {
+    const { name } = useParams();
+    return(
+      <div>
+        <h2>User Params Ejemplo</h2>
+        <div>{name}</div>
+      </div>
+    )
+  }
+
+export {Home,About,Dashboard} ;
+
+/* 
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+const ExploreDetails = ({ data }) => {
+  const { name } = useParams();
+
+  return (
+      <div className="full-detail">
+        <div className="explore-container">
+          {
+            data
+              .filter((list) => list.name === name)
+              .map((list) => (
+                <div className="full-card" key={ list.id }>
+                  <h2>Name: {list.name}</h2>
+                  <h4>Category: {list.category}</h4>
+                </div>
+              ))}
+        </div>
+      </div>
+  )
+}
+
+export default ExploreDetails
+ */
